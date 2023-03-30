af2967c11c87200e05a62df87d147c280a557521
FaxWeb <80632493+FaxWeb7@users.noreply.github.com>
2023-03-30 18:23:37 +0300

    font load

--- App.js
+++ App.js
@@ -1,21 +1,45 @@
-import { StyleSheet, View } from 'react-native';
 import HomeScreen from './screens/HomeScreen';
+import { useCallback, useEffect, useState } from 'react';
 import * as Font from 'expo-font'
-import { useState } from 'react';
-import AppLoading from 'expo-app-loading'
+import * as SplashScreen from 'expo-splash-screen';
+import { View } from 'react-native';
 
-const fonts = () => Font.loadAsync({
-  'mt-semibold': require('./assets/fonts/Montserrat-SemiBold.ttf'),
-  'mt-medium': require('./assets/fonts/Montserrat-Medium.ttf'),
-  'mt-regular': require('./assets/fonts/Montserrat-Regular.ttf'),
-  'mt-light': require('./assets/fonts/Montserrat-Light.ttf'),
-})
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
-  const [font, setFont] = useState(false)
-  if (font) {
-    return <HomeScreen /> 
-  } else {
-    return <AppLoading startAsync={fonts} onFinish={() => setFont(true)} onError={(e) => console.log(e)} /> 
+  const [appIsReady, setAppIsReady] = useState(false)
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+        await Font.loadAsync({
+          'mt-semibold': require('./assets/fonts/Montserrat-SemiBold.ttf'),
+          'mt-medium': require('./assets/fonts/Montserrat-Medium.ttf'),
+          'mt-regular': require('./assets/fonts/Montserrat-Regular.ttf'),
+          'mt-light': require('./assets/fonts/Montserrat-Light.ttf'),
+        })
+        await new Promise(resolve => setTimeout(resolve, 2000));
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        setAppIsReady(true);
+      }
+    }
+
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return null;
   }
+
+  return (
+    <View onLayout={onLayoutRootView}><HomeScreen /></View>
+  )
 }
